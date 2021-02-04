f9cb581a4fc996076f294bb12d69399bf27e8c54
Pavan S Matkar <67954302+PavanMatkar@users.noreply.github.com>
2021-02-05 00:06:32 +0530

    day-1

---
+++ Day1.js
@@ -0,0 +1,44 @@
+/*
+- React is a Library.
+- Used by Facebook.
+
+- React JS (Web applications)
+- React Native (Mobile application)
+- React Bootstrap (Rich UI to web app & mobile app)
+
+Web applications 2 Approach
+1) JSX [Javascript + XML]       ES5 - ES11 Not support OOPS [object oriented programming]
+2) Typescript     
+- typescript is a programming language.
+- superset of Javascript. 
+- supports OOPS.
+
+* Topics to be covered in course
+1) Databases Communications
+    -> MongoDB
+        (No SQL)
+    -> MySQL
+    -> SQL Server
+    -> Oracle
+2) MERN Stack Development
+    M - Mongo DB
+    E - Express JS
+    R - React JS
+    N - Node JS
+3) Middle wares
+    -> Redux
+    -> Saga
+    -> Thunk
+4) Deployments  (Cloud Deployments)
+    -> AWS
+    -> Heroku
+5) Project Live Deployment
+    -> Ecommerce Application
+    -> 2nd Application
+6) Integrations
+    Spring Boot Integration
+
+    Throttling Concept
+    Debouncing
+    Currying
+*/
