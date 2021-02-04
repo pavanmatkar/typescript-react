fda81dc2f97bbfe48b0304a4bf34c842d11e7f8a
Pavan S Matkar <67954302+PavanMatkar@users.noreply.github.com>
2021-02-05 00:30:17 +0530

    day-2

---
+++ Day2.js
@@ -0,0 +1,60 @@
+/*
+- Typescript is the programming language.
+- Miscrsoft released it.
+- Superset of Javascript is called Typescript.
+- Typescript supports the OOPS.
+- Extension of typescript files is ".ts"
+- Recommended IDE to build Typescript is "Visual Studio Code"
+- "Visual Studio Code" IDE is open source IDE provided by Microsoft.
+- Typescript used to build the Angular, React, Node and VueJS,.....Applications.
+- Once if we build above Applications with Typescript automatically we are getting benefits of OOPS.
+var X = 100;        -JS
+var X : number = 100;       -TS
+- Typescript is the strictly typed language.
+- In general browsers can't understand TS.
+- So as a Developer we must convert TS to equivalent JS.
+- Converting Typescript to equivalent JS called as "Transpilation".
+- "tsc" is the tool used to perform the Transpilation.
+- "tsc" stands for Typescript Compiler.f
+- we will install Typescript with the help of "npm" tool.
+- "npm" stands for "node packaging manager".
+- "npm" present in "NodeJS"
+
+Installation of Softwares.
+1) Download & install nodeJS
+website:    https://nodejs.org/en/download
+file:   node -v 14.15.1 * 64.msi
+- msi stands for microsoft installer.
+2) install Typescript
+- we will install Typescript by using following command.
+> npm install -g typescript
+- npm present in "NodeJS"
+- "-g" stands for global installation
+3) download & install Visual Studio Code
+website:    https://code.visualstudio.com/docs/?dv=win
+
+Transpilation
+- converting typescript to equivalent Javascript called as Transpilation.
+- "tsc" is the tool used to perform the transpilation.
+    > tsc demo.ts
+    > node demo.js
+
+#Variables
+- Variables are used to store the data.
+    Ex. 
+        number
+        string
+        boolean
+        arrays
+        objects
+        ----
+        ----
+        ----
+- we will declare the variables by using "var", "let" or "const".
+- "let" & "const" keywords introduced in ES6.
+- variables should contain a-z, A-Z, 0-9, $ and _
+- variables should not start with numbers.
+
+# syntax
+var/let/const variablename : datatype = value;
+*/ 
