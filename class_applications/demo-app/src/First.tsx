8ed18aa3f6a8ac32eb9e16648bbcc2fd2d8e274f
Pavan S Matkar <pavanmatkar123@gmail.com>
2021-02-21 10:25:02 +0530

    demo-app

---
+++ class_applications/demo-app/src/First.tsx
@@ -0,0 +1,43 @@
+//import React Class
+//React class provides the Component environment
+//React class available in "react" package
+import React from "react";
+/*
+    //create the function
+    function First(){
+        return(
+            <h1>Welcome to React Stateless Component!!!</h1>
+        )
+    }
+
+    //export the Component
+    export default First;
+*/
+/*
+    export default function First(){
+        return(
+            <React.Fragment>
+                <h1>React!!!</h1>
+                <h1>Encapsulation!!!</h1>
+            </React.Fragment>
+        );
+    };
+*/
+
+// export const First = () => (<h1>Welcome to React!!!</h1>);
+/*
+    export const First = () => (<React.Fragment>
+        <h1>Hello_1</h1>
+        <h2>Hello_2</h2>
+    </React.Fragment>)
+*/
+/*
+    export const First = () => {
+        return(
+            <React.Fragment>
+                <h1>Hello_1</h1>
+                <h2>Hello_2</h2>
+            </React.Fragment>
+        );
+    };
+*/
