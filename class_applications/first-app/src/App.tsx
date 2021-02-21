6dc20f5eb46f8507d7929d239b8d88ab1e2b5330
Pavan S Matkar <pavanmatkar123@gmail.com>
2021-02-21 10:25:53 +0530

    first-app

---
+++ class_applications/first-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to ReactJS with Typescript....!</h1>
+    </div>
+  );
+}
+
+export default App;
